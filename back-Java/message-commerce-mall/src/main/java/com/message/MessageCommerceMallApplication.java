c3ef27ab233950a14f7631f36fcd414d82988b77
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-08-12 00:30:24 +0900

    ㅎㅎ

---
+++ back-Java/message-commerce-mall/src/main/java/com/message/MessageCommerceMallApplication.java
@@ -0,0 +1,13 @@
+package com.message.messagecommercemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageCommerceMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageCommerceMallApplication.class, args);
+	}
+
+}
---
+++ back-Java/message-commerce-mall/src/test/java/com/message/messagecommercemall/MessageCommerceMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message.messagecommercemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageCommerceMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
